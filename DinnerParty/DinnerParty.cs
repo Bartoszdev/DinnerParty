da308bff8e1271e73808603b800d4adea62fd9b4
Bartek <bartosz@e-mo.pl>
2018-04-17 23:48:07 +0200

    1# Commit

---
+++ DinnerParty/DinnerParty.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinnerParty
+{
+    public class DinnerParty
+    {
+        public int NumberOfPeople = 0;
+        decimal CostOfBeveragesPerPerson = 0.0M;
+        decimal CostOfDecoration = 0.0M;
+        public const int CostOfFoodPerPerson = 25;
+        public void SetHealthyOption(bool healthyOption)
+        {
+            if(healthyOption)
+            {
+                CostOfBeveragesPerPerson = 5;
+
+                CostOfBeveragesPerPerson += CostOfFoodPerPerson;
+
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = 20;
+
+                CostOfBeveragesPerPerson += CostOfFoodPerPerson;
+
+            }
+
+        }
+
+        public void CalculateCostOfDecorations(bool fancyOption)
+        {
+
+        }
+        public decimal CalculateCost()
+        {
+            
+
+        }
+
+    }
+}
--- DinnerParty/Form1.cs
+++ DinnerParty/Form1.cs
@@ -12,9 +12,41 @@ namespace DinnerParty
 {
     public partial class Form1 : Form
     {
+        
+        DinnerParty dinnerparty;
         public Form1()
         {
             InitializeComponent();
+
+            dinnerparty = new DinnerParty { NumberOfPeople = 5 };
+            dinnerparty.SetHealthyOption(false);
+            dinnerparty.CalculateCostOfDecorations(true);
+            DisplayDinnerPartyCost()
+        }
+
+        public void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerparty.CalculateCost();
+            costLabel.Text = Cost.ToString("c");
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerparty.NumberOfPeople = (int)numericUpDown1.Value;
+            DisplayDinnerPartyCost();
+        }
+
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerparty.CalculateCostOfDecorations(fancyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void healthyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerparty.SetHealthyOption(healthyBox.Checked);
+            DisplayDinnerPartyCost()
         }
     }
 }
